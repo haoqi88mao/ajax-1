dc3823208ff1be7f627922e39da358234fde9821
Azao <958695631@qq.com>
2020-02-08 13:43:51 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,90 @@
+console.log("我是main.js");
+let n = 1;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const array = JSON.parse(request.response);
+      array.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+    }
+  };
+  request.send();
+};
+getJson.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const bool = JSON.parse(request.response);
+      myName.textContent = object.name;
+      console.log(bool);
+    }
+  };
+  request.send();
+};
+getXml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      const dom = request.responseXML;
+      const text = dom.getElementsByTagName("warning")[0].textContent;
+      console.log(text.trim());
+    }
+  };
+  request.send();
+};
+getHtml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    //创建div标签
+    const div = document.createElement("div");
+    // 填写div内容
+    div.innerHTML = request.response;
+    // 插入到身体里
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+getJs.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    //   创建script标签
+    const script = document.createElement("script");
+    // 填写script内容
+    script.innerHTML = request.response;
+    // 插到身体里
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {};
+  request.send();
+};
+getCss.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "style.css");
+  request.onreadystatechange = () => {
+    //   下载完成,但不知道是成功还是失败
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        // 创建style标签
+        const style = document.createElement("style");
+        // 填写style内容
+        style.innerHTML = request.response;
+        // 插到头里面
+        document.head.appendChild(style);
+      } else {
+        alert("加载css失败");
+      }
+    }
+  };
+  request.send();
+};
